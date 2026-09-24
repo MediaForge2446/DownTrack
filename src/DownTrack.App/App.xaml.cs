f3e79f263ed619198fb5d739ea4cf33c0ba30747
MediaForge2446 <popera541@gmail.com>
2026-09-24 12:37:54 +0300

    fix: wire media engine into commit service

--- src/DownTrack.App/App.xaml.cs
+++ src/DownTrack.App/App.xaml.cs
@@ -24,12 +24,12 @@ public partial class App : System.Windows.Application
             var stateStore = new JsonAppStateStore(AppPaths.StateFile);
             var processRunner = new ProcessRunner();
             var locator = new ToolLocator();
+            var toolManager = new ToolManager();
             var downloader = new YtDlpDownloader(locator, processRunner);
-            var commitService = new CommitService(downloader);
+            var commitService = new CommitService(downloader, toolManager);
             var staging = new StagingService(stateStore, commitService);
             var folderPicker = new WindowsFolderPicker();
             var prompt = new WpfTextPromptService();
-            var toolManager = new ToolManager();
             var resolver = new YouTubeMetadataResolver(locator, processRunner);
             var mediaDialog = new WpfMediaDialogService(resolver);
 
