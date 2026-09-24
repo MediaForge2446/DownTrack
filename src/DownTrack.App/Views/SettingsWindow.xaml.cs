d755d7a57c981945815be39a7655784a0c330772
MediaForge2446 <popera541@gmail.com>
2026-09-24 19:16:04 +0300

    feat: add language settings window

---
+++ src/DownTrack.App/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,36 @@
+using System.Windows;
+using System.Windows.Input;
+using DownTrack.Infrastructure.Localization;
+
+namespace DownTrack.Views;
+
+public partial class SettingsWindow : Window
+{
+    private readonly LocalizationService _localization;
+
+    public SettingsWindow()
+    {
+        InitializeComponent();
+        _localization = LocalizationService.Instance;
+        DataContext = _localization;
+        Loaded += (_, _) => LanguageBox.Focus();
+    }
+
+    private void Apply_Click(object sender, RoutedEventArgs e)
+    {
+        _localization.SetLanguage(LanguageBox.SelectedValue as string ?? "en");
+        DialogResult = true;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e) =>
+        DialogResult = false;
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.OriginalSource is System.Windows.Controls.Button)
+            return;
+
+        if (e.LeftButton == MouseButtonState.Pressed)
+            DragMove();
+    }
+}
