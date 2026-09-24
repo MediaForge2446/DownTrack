b3233f9d17ba150cae3966b65094c9058009697a
MediaForge2446 <popera541@gmail.com>
2026-09-24 23:22:00 +0300

    i18n: localize media mode labels

--- src/DownTrack.App/Infrastructure/Windows/BooleanModeConverter.cs
+++ src/DownTrack.App/Infrastructure/Windows/BooleanModeConverter.cs
@@ -1,12 +1,15 @@
 using System.Globalization;
 using System.Windows.Data;
+using DownTrack.Infrastructure.Localization;
 
 namespace DownTrack.Infrastructure.Windows;
 
 public sealed class BooleanModeConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-        value is true ? "Video" : "Audio";
+        value is true
+            ? LocalizationService.Instance.T("Media.Video")
+            : LocalizationService.Instance.T("Media.Audio");
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
         Binding.DoNothing;
